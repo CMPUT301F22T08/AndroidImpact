10f6ad3a46fc9954a11e8ad34e47417eebaa2ba5
Joshua Ji <joshua.ji@jiwhiz.com>
2022-10-21 13:32:21 -0600

    Added recycler view from Lab 5

---
+++ app/src/main/java/com/androidimpact/app/Ingredient.java
@@ -0,0 +1,13 @@
+package com.androidimpact.app;
+
+public class Ingredient {
+    private String description;
+
+    Ingredient(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
