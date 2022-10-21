17326704aee402970444f901f81266f5eaac6abc
Kailash Seshadri <kailashseshadri2002@gmail.com>
2022-10-21 13:27:50 -0600

    Added ingredient class

---
+++ app/src/main/java/com/androidimpact/app/Ingredient.java
@@ -0,0 +1,68 @@
+package com.androidimpact.app;
+
+public class Ingredient {
+    protected String description;
+    protected float amount;
+    protected String unit;
+    protected String category;
+
+    public Ingredient(String description, float amount, String unit, String category) {
+        this.description = description;
+        this.amount = amount;
+        this.unit = unit;
+        this.category = category;
+    }
+
+    public Ingredient(String description, double amount, String unit, String category) {
+        this.description = description;
+        this.amount = (float) amount;
+        this.unit = unit;
+        this.category = category;
+    }
+
+    public Ingredient(String description, int amount, String unit, String category) {
+        this.description = description;
+        this.amount = (float) amount;
+        this.unit = unit;
+        this.category = category;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = (float) amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = (float) amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
