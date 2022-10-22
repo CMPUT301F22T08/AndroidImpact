e05e71c65a19ccb6826cb523113653b231b04112
Joshua Ji <joshua.ji@jiwhiz.com>
2022-10-22 16:33:34 -0600

    Init: `IngredientStorageAdd.java`

---
+++ app/src/main/java/com/androidimpact/app/IngredientStorageAdd.java
@@ -0,0 +1,14 @@
+package com.androidimpact.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngredientStorageAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_storage_add);
+    }
+}
