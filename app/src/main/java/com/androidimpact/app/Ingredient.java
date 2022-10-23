48876d44036a6925d9e315b1235661ef83504874
Kailash Seshadri <kailashseshadri2002@gmail.com>
2022-10-23 00:23:37 -0600

    Added Javadoc for Ingredient.java

--- app/src/main/java/com/androidimpact/app/Ingredient.java
+++ app/src/main/java/com/androidimpact/app/Ingredient.java
@@ -1,7 +1,13 @@
 package com.androidimpact.app;
 
+/**
+ * Ingredient class: Holds the information of an ingredient to be used in recipes and the shopping list
+ * - description - A short description of the ingredient e.g. peppercorn ranch
+ * - amount - The quantity of the ingredient needed for the recipe/shopping list e.g. 300 in 300g
+ * - unit - The unit that amount is measuring e.g. g in 300g
+ * - category - Any name that helps categorize the ingredient e.g. Sauce for peppercorn ranch
+ */
 public class Ingredient {
-
     protected String description;
     protected float amount;
     protected String unit;
@@ -14,34 +20,66 @@ public class Ingredient {
         this.category = category;
     }
 
+    /**
+     * Getter for ingredient description
+     * @return this.description (String)
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Rename the ingredient
+     * @param description (String) - The new description to be used
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Get the amount of the ingredient
+     * @return this.amount (float)
+     */
     public float getAmount() {
         return amount;
     }
 
+    /**
+     * Set a new amount of the ingredient
+     * @param amount (float) - The new amount to be used
+     */
     public void setAmount(float amount) {
         this.amount = amount;
     }
 
+    /**
+     * Get the units that amount is measuring
+     * @return this.unit (String)
+     */
     public String getUnit() {
         return unit;
     }
 
+    /**
+     * Set a new unit of measurement
+     * @param unit (String) - The new unit of measurement
+     */
     public void setUnit(String unit) {
         this.unit = unit;
     }
 
+    /**
+     * Get the ingredient category
+     * @return this.category (String)
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Set a new ingredient category
+     * @param category (String) - The new category of the ingredient
+     */
     public void setCategory(String category) {
         this.category = category;
     }
--- app/src/main/java/com/androidimpact/app/MainActivity.java
+++ app/src/main/java/com/androidimpact/app/MainActivity.java
@@ -10,8 +10,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.CollectionReference;
@@ -29,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
     // adding cities to firebase
     final String TAG = "MainActivity";
-    Button addIngredientBtn;
-    EditText addIngredientDescriptionText;
     FirebaseFirestore db;
 
     @Override
