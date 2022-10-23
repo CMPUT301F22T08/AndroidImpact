e580735c103001a2d548186313ac88faa70cee66
Kailash Seshadri <kailashseshadri2002@gmail.com>
2022-10-23 16:35:51 -0600

    Created tests for bestBefore, updated tests for other fields. Updated snackbar to display errors

--- app/src/main/java/com/androidimpact/app/AddStoreIngredientActivity.java
+++ app/src/main/java/com/androidimpact/app/AddStoreIngredientActivity.java
@@ -38,7 +38,6 @@ public class AddStoreIngredientActivity extends AppCompatActivity {
     private Button confirmBtn;
 
     // Misc
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +69,7 @@ public class AddStoreIngredientActivity extends AppCompatActivity {
             bestBefore = cal.getTime();
 
             // now, update best before date
-            String myFormat="dd/MM/yy";
+            String myFormat="dd-MM-yyyy";
             SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
             bestBeforeEdit.setText(dateFormat.format(cal.getTime()));
         };
@@ -95,48 +94,90 @@ public class AddStoreIngredientActivity extends AppCompatActivity {
      * This is executed when the "confirm" button is clicked
      */
     public void confirm(View view) {
-        Log.i(TAG + ":cancel", "Confirm ingredient add");
+       Log.i(TAG + ":cancel", "Confirm ingredient add");
 
-        // right now, just make a snackbar saying it's not finished
+       String snackbarStr = "Not implemented!";
+       try {
+           StoreIngredient newStoreIngredient = createIngredient();
+       } catch (Exception e){
+           snackbarStr = e.getMessage();
+       }
+
+        // right now, just make a snack-bar saying the error or not implemented
         View parentLayout = findViewById(android.R.id.content);
-        Snackbar.make(parentLayout, "Not implemented!", Snackbar.LENGTH_LONG)
+        Snackbar.make(parentLayout, snackbarStr, Snackbar.LENGTH_LONG)
                 .setAction("Ok", view1 -> {})
                 .show();
     }
 
     /**
-     * Attempts to create an ingredient with the values in the inputs.
-     * Throws an exception if it fails.
-     * @return
+     * Validates the data input by the user
+     * Create a StoreIngredient object if the data is valid.
+     * @return A StoreIngredient object created using the data from the editTexts
+     * @throws Exception if the data is invalid or if the Ingredient could not be created
+     * @see StoreIngredient
      */
-    private Ingredient createIngredient() throws Exception {
-        String description = descriptionEdit.toString();
-        String amountRaw = amountEdit.toString();
-        String location = locationEdit.toString();
-        String unit = unitEdit.toString();
-        String category = categoryEdit.toString();
+    // Should we throw an exception or have a snack-bar that says fields can't be empty?
+    private StoreIngredient createIngredient() throws Exception {
+        String description = descriptionEdit.getText().toString();
+        String amountRaw = amountEdit.getText().toString();
+        String location = locationEdit.getText().toString();
+        String unit = unitEdit.getText().toString();
+        String category = categoryEdit.getText().toString();
+        String bestBeforeDate = bestBeforeEdit.getText().toString();
 
         if (description.equals("")) {
-            throw new Exception("description");
+            throw new Exception("Description cannot be empty.");
         }
 
         if (amountRaw.equals("")) {
-            throw new Exception("amount");
+            throw new Exception("Amount cannot be empty.");
+        }
+
+        float amount;
+        try {
+            amount = Float.parseFloat(amountRaw);
+            assert amount>0;
+        } catch (Exception e) {
+            throw new Exception("Amount must be positive.");
         }
 
         if (location.equals("")) {
-            throw new Exception("amount");
-        }
-        if (unit.equals("")) {
-            throw new Exception("amount");
-        }
-        if (category.equals("")) {
-            throw new Exception("amount");
+            throw new Exception("Location cannot be empty.");
         }
 
+        if (unit.equals("")) {
+            throw new Exception("Unit cannot be empty.");
+        }
+
+        if (category.equals("")) {
+            throw new Exception("Category cannot be empty.");
+        }
+
+        if (bestBeforeDate.equals("")) {
+            throw new Exception("Best before date cannot be empty.");
+        }
         try {
-            int amount = Integer.parseInt(amountRaw);
-            return new Ingredient(description, amount, unit, category);
+            String[] ddMMyy = bestBeforeDate.split("-", 3);
+            assert (ddMMyy.length == 3);
+        } catch (Exception e) {
+            throw new Exception("Best before date must be dd-MM-yyyy.");
+        }
+
+        // Check if best before date is today, or a future date.
+        Calendar today = Calendar.getInstance();
+        Calendar bestBefore = Calendar.getInstance();
+        String[] ddMMyy = bestBeforeDate.split("-", 3);
+        bestBefore.set(Calendar.DAY_OF_MONTH, Integer.parseInt(ddMMyy[0]));
+        bestBefore.set(Calendar.MONTH, Integer.parseInt(ddMMyy[1]));
+        bestBefore.set(Calendar.YEAR, Integer.parseInt(ddMMyy[2]));
+        if (bestBefore.compareTo(today) <= 0) {
+            throw new Exception("Best before date must be a future date.");
+        }
+
+
+        try {
+            return new StoreIngredient(description, amount, unit, category, bestBefore, location);
         } catch(Exception e) {
             throw new Exception("Error parsing ingredients");
         }
