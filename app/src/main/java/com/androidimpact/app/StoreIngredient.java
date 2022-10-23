fdff3ff1c8c981c012cda5b091135c80988a6542
Vedant Vyas <53002423+vedudx@users.noreply.github.com>
2022-10-23 00:50:00 -0600

    Refactoring main activity.java to account for change from IngredientViewAdapter to StoreIngredientViewAdapter

--- app/src/main/java/com/androidimpact/app/StoreIngredient.java
+++ app/src/main/java/com/androidimpact/app/StoreIngredient.java
@@ -1,2 +1,30 @@
-package com.androidimpact.app;public class StoreIngredient {
+package com.androidimpact.app;
+
+import java.util.Date;
+
+public class StoreIngredient extends Ingredient{
+    private Date bestBeforeDate;
+    private String location;
+
+    public StoreIngredient(String description, float amount, String unit, String category, Date bestBeforeDate, String location){
+        super(description, amount, unit, category);
+        this.bestBeforeDate = bestBeforeDate;
+        this.location = location;
+    }
+
+    public Date getBestBeforeDate() {
+        return bestBeforeDate;
+    }
+
+    public void setBestBeforeDate(Date bestBeforeDate) {
+        this.bestBeforeDate = bestBeforeDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
