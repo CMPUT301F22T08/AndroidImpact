1fcb4d07d9e2f75a58b978a10edc5edb5bdf0806
Joshua Ji <joshua.ji@jiwhiz.com>
2022-10-23 00:15:29 -0600

    Tests: fix example isntrument test

--- app/src/androidTest/java/com/androidimpact/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/androidimpact/app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.androidimpact", appContext.getPackageName());
+        assertEquals("com.androidimpact.app", appContext.getPackageName());
     }
 }
