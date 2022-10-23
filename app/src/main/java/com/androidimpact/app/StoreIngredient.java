355bc640e6587ae05cc52135c8e1e8440824d17a
Kailash Seshadri <kailashseshadri2002@gmail.com>
2022-10-23 15:51:09 -0600

    Refactor `Date` -> `Calendar`

--- app/src/main/java/com/androidimpact/app/StoreIngredient.java
+++ app/src/main/java/com/androidimpact/app/StoreIngredient.java
@@ -1,19 +1,19 @@
 package com.androidimpact.app;
 
-import java.util.Date;
+import java.util.Calendar;
 
 /**
  * StoreIngredient object that is stored in IngredientStorage
  * Extended from Ingredient
  * Extra attributes:
- *  - bestBeforeDate (Date) - The best before date for the stored ingredient
+ *  - bestBeforeDate (Calendar) - The best before date for the stored ingredient
  *  - location (String) - Where the ingredient is stored e.g. Pantry or Freezer
  * @author Kailash Seshadri
  * @version 1.0
  * @see Ingredient
  */
 public class StoreIngredient extends Ingredient{
-    private Date bestBeforeDate;
+    private Calendar bestBeforeDate;
     private String location;
 
     /**
@@ -22,11 +22,11 @@ public class StoreIngredient extends Ingredient{
      * @param amount (float) - The quantity of the ingredient needed for the recipe/shopping list e.g. 300 in 300g
      * @param unit (String) - The unit that amount is measuring e.g. g in 300g
      * @param category (String) - Any name that helps categorize the ingredient e.g. Sauce for peppercorn ranch
-     * @param bestBeforeDate (Date) - The best before date for the stored ingredient
+     * @param bestBeforeDate (Calendar) - The best before date for the stored ingredient
      * @param location (String) - Where the ingredient is being stored
      * @see Ingredient
      */
-    public StoreIngredient(String description, float amount, String unit, String category, Date bestBeforeDate, String location){
+    public StoreIngredient(String description, float amount, String unit, String category, Calendar bestBeforeDate, String location){
         super(description, amount, unit, category);
         this.bestBeforeDate = bestBeforeDate;
         this.location = location;
@@ -34,17 +34,17 @@ public class StoreIngredient extends Ingredient{
 
     /**
      * Get the bestBeforeDate of the stored ingredient
-     * @return (Date) The best before date for the stored ingredient
+     * @return (Calendar) The best before date for the stored ingredient
      */
-    public Date getBestBeforeDate() {
+    public Calendar getBestBeforeDate() {
         return bestBeforeDate;
     }
 
     /**
      * Change the best-before date of the stored ingredient
-     * @param bestBeforeDate (Date) - The new best-before date of the stored ingredient
+     * @param bestBeforeDate (Calendar) - The new best-before date of the stored ingredient
      */
-    public void setBestBeforeDate(Date bestBeforeDate) {
+    public void setBestBeforeDate(Calendar bestBeforeDate) {
         this.bestBeforeDate = bestBeforeDate;
     }
 
