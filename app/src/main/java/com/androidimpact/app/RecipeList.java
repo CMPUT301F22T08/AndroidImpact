07dae0e5aa2e940967958f9b43845f3e46d289e6
Aneeljyot Alagh <69876558+ajsingh1012@users.noreply.github.com>
2022-10-23 16:23:45 -0600

    small change of indenting in documentation

--- app/src/main/java/com/androidimpact/app/RecipeList.java
+++ app/src/main/java/com/androidimpact/app/RecipeList.java
@@ -1,2 +1,10 @@
-package com.androidimpact.app;public class RecipeList {
+package com.androidimpact.app;
+
+/**
+ * This class defines a recipe list
+ * @author Aneeljyot Alagh, Clare Chen
+ * @version 1.0
+ */
+public class RecipeList {
+
 }
