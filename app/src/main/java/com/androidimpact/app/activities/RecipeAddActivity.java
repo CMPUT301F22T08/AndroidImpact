f4fc09823692b155503693f29e3c8d45b7eaf151
Curtis Kan <curtis.tl.kan@gmail.com>
2022-10-23 20:52:42 -0600

    added package for activities for better organization of classes

---
+++ app/src/main/java/com/androidimpact/app/activities/RecipeAddActivity.java
@@ -0,0 +1,14 @@
+package com.androidimpact.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_add);
+    }
+}
---
+++ app/src/main/java/com/androidimpact/app/activities/RecipeViewActivity.java
@@ -0,0 +1,14 @@
+package com.androidimpact.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+}
