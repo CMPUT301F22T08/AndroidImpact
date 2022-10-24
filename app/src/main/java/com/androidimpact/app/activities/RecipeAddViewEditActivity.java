22cf3840a83a7c69ad722da12f43aa4ed33b2025
Curtis Kan <curtis.tl.kan@gmail.com>
2022-10-23 22:56:06 -0600

    initial UI for ingredients add/view/edit

--- app/src/main/java/com/androidimpact/app/activities/RecipeAddViewEditActivity.java
+++ app/src/main/java/com/androidimpact/app/activities/RecipeAddViewEditActivity.java
@@ -1,14 +1,16 @@
-package com.androidimpact.app;
+package com.androidimpact.app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RecipeViewActivity extends AppCompatActivity {
+import com.androidimpact.app.R;
+
+public class RecipeAddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_view);
+        setContentView(R.layout.activity_recipe_add);
     }
 }
