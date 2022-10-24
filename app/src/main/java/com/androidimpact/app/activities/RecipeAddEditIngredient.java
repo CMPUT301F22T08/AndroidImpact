31a9e0eb0326e21b7140a1134bf6090c306f7c0f
Curtis Kan <curtis.tl.kan@gmail.com>
2022-10-24 00:02:34 -0600

    added initial add/edit ingredient UI

---
+++ app/src/main/java/com/androidimpact/app/activities/RecipeAddEditIngredient.java
@@ -0,0 +1,14 @@
+package com.androidimpact.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeAddEditIngredient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_add_edit_ingredient);
+    }
+}
