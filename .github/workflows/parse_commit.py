b2b4edcb97a9e096dd79ff74963e4fbaad7c5bf1
Joshua Ji <joshua.ji@jiwhiz.com>
2022-10-24 19:52:01 -0600

    Parse commit message
    only use the first line

--- .github/workflows/parse_commit.py
+++ .github/workflows/parse_commit.py
@@ -5,9 +5,9 @@ import glob
 import time
 
 def main():
-    terminalArgument = sys.argv                    #creating alias to sys argv
     commitMessage = sys.argv[1]
     print(commitMessage.strip().split("\n")[0])
+
 if __name__ == "__main__":
     main()
 
