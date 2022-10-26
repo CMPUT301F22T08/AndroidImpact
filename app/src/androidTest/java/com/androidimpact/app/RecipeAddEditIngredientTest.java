ab6d1846b7cfafdcac2c4a1cda36797da1dde4ea
Joshua Ji <joshuanianji@users.noreply.github.com>
2022-10-26 15:15:04 -0600

    Add Recipes (#51)
    * unit tests didnt add yesterday
    
    * adding functionality to UI and connecting recipe to database
    
    * linked intents, most functionality done
    
    * should be sending info to database now
    
    * tests: fix tests
    
    Co-authored-by: Curtis Kan <curtis.tl.kan@gmail.com>

--- app/src/androidTest/java/com/androidimpact/app/RecipeAddEditIngredientTest.java
+++ app/src/androidTest/java/com/androidimpact/app/RecipeAddEditIngredientTest.java
@@ -1,2 +1,19 @@
-package com.androidimpact.app;public class RecipeAddEditIngredientTest {
+package com.androidimpact.app;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class RecipeAddEditIngredientTest {@Test
+public void useAppContext() {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    assertEquals("com.androidimpact.app", appContext.getPackageName());
+}
 }
--- app/src/androidTest/java/com/androidimpact/app/RecipeAddViewEditTest.java
+++ app/src/androidTest/java/com/androidimpact/app/RecipeAddViewEditTest.java
@@ -1,2 +1,18 @@
-package com.androidimpact.app;public class RecipeAddViewEditTest {
+package com.androidimpact.app;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+
+public class RecipeAddViewEditTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.androidimpact.app", appContext.getPackageName());
+    }
 }
