403bd5c7defd1ce9504b1f2bf31681ca2c86c44f
Vedant Vyas <53002423+vedudx@users.noreply.github.com>
2022-10-26 12:09:25 -0600

    Finishing the test cases for ingredient storage. I was finally able to fix my android studio. All these test passed in my laptop

--- app/src/test/java/com/androidimpact/app/IngredientStorageTest.java
+++ app/src/test/java/com/androidimpact/app/IngredientStorageTest.java
@@ -1,6 +1,8 @@
 package com.androidimpact.app;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
@@ -26,6 +28,97 @@ public class IngredientStorageTest {
         StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
         ingredientList.add(ingredient);
         assertEquals(2, ingredientList.size());
+    }
+
+
+    // Need some clarification for this error from team so will fix it later
+    @Test
+    public void testAddException() {
+        IngredientStorage ingredientList = mockIngredientList();
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.add(ingredient);
+        assertEquals(2, ingredientList.size());
+        ingredientList.add(ingredient);
+        assertEquals(3, ingredientList.size());
+    }
+    @Test
+    public void testGetIngredient() {
+        IngredientStorage ingredientList = mockIngredientList();
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.add(ingredient);
+        //to add a compareTo method in store ingredient
+        assertEquals(true, ingredient == ingredientList.get(1));
+
+        assertEquals(false, ingredient == ingredientList.get(0));
+    }
+
+    @Test
+    public void testSetIngredient() {
+        IngredientStorage ingredientList = mockIngredientList();
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.set(0, ingredient);
+        //to add a compareTo method in store ingredient
+        assertEquals(true, ingredient == ingredientList.get(0));
+
+    }
+
+    @Test
+    public void testRemove()
+    {
+        IngredientStorage ingredientList = mockIngredientList();
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.add(ingredient);
+        assertEquals(2, ingredientList.size());
+        ingredientList.remove(ingredient);
+        assertEquals(1, ingredientList.size());
+
+        // test deleting invalid store ingredient
+        assertThrows( IllegalArgumentException.class, () -> {
+            ingredientList.remove(new StoreIngredient("egg1", 10, "", "plt", new Date(), "counter")); });
+
+        assertEquals(1, ingredientList.size());
+    }
+
+
+    @Test
+    public void testClear()
+    {
+        IngredientStorage ingredientList = mockIngredientList();
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.add(ingredient);
+        assertEquals(2, ingredientList.size());
+        ingredientList.clear();
+        assertEquals(0, ingredientList.size());
+
+    }
+    @Test
+    public void testSize()
+    {
+        IngredientStorage ingredientList = mockIngredientList();
+        assertEquals(1, ingredientList.size());
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.add(ingredient);
+        assertEquals(2, ingredientList.size());
+        ingredientList.clear();
+        assertEquals(0, ingredientList.size());
+
+    }
+
+    @Test
+    public void testRemoveInt()
+    {
+        IngredientStorage ingredientList = mockIngredientList();
+        StoreIngredient ingredient = new StoreIngredient("egg", 10, "", "poultry",new Date(), "fridge");
+        ingredientList.add(ingredient);
+        assertEquals(2, ingredientList.size());
+        ingredientList.remove(1);
+        assertEquals(1, ingredientList.size());
+
+        // test deleting invalid city
+        assertThrows( ArrayIndexOutOfBoundsException.class, () -> {
+            ingredientList.remove(5); });
+
+        assertEquals(1, ingredientList.size());
     }
 
 }
