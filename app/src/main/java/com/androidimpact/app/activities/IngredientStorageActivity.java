55aba76a09c384fa1ca2396516846f8810aa391f
Kailash Seshadri <87671876+kailashs2002@users.noreply.github.com>
2022-10-28 18:33:45 -0600

    Cleanup

--- app/src/main/java/com/androidimpact/app/activities/IngredientStorageActivity.java
+++ app/src/main/java/com/androidimpact/app/activities/IngredientStorageActivity.java
@@ -71,7 +71,7 @@ public class IngredientStorageActivity extends AppCompatActivity {
         });
 
 
-       // drag to delete
+        // drag to delete
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
 
             @Override
