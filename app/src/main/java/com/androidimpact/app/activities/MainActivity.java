27efd31f9cb3ea255fb46976a3a1e98c45a535e3
Vedant Vyas <53002423+vedudx@users.noreply.github.com>
2022-11-06 01:03:11 -0600

    Fixed multiple snapshot listener and improvised the new instance method

--- app/src/main/java/com/androidimpact/app/activities/MainActivity.java
+++ app/src/main/java/com/androidimpact/app/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidimpact.app.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -18,6 +19,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     // adding cities to firebase
     final String TAG = "MainActivity";
 
+    final IngredientStorageFragment storageFragment = IngredientStorageFragment.newInstance();
+    final ShoppingListFragment shoppingListFragment = new ShoppingListFragment();
+    final  MealPlannerFragment mealPlannerFragment = new MealPlannerFragment();
+    final  RecipeListFragment recipeListFragment = new RecipeListFragment();
+    Fragment active = storageFragment;
+
 
     BottomNavigationView bottomnav;
     @Override
@@ -33,14 +40,15 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomnav.setOnNavigationItemSelectedListener(this);
         bottomnav.setSelectedItemId(R.id.storage_icon);
 
+        getSupportFragmentManager().beginTransaction().add(R.id.nav_fragment, recipeListFragment, "2").hide(recipeListFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.nav_fragment, shoppingListFragment, "3").hide(shoppingListFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.nav_fragment, mealPlannerFragment, "4").hide(shoppingListFragment).commit();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.nav_fragment, storageFragment, "1").commit();
+
 
         //navController = findNavController(R.id.nav_fragment)
     }
-
-
-    ShoppingListFragment shoppingListFragment = new ShoppingListFragment();
-    MealPlannerFragment mealPlannerFragment = new MealPlannerFragment();
-
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -48,24 +56,32 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             case R.id.storage_icon:
                 //IngredientStorage storageFragment = new IngredientStorage();
                 getSupportActionBar().setTitle("Ingredient Storage");
-                IngredientStorageFragment storageFragment = IngredientStorageFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, storageFragment, "STORAGE").commit();
+              //  IngredientStorageFragment storageFragment = IngredientStorageFragment.newInstance();
+                getSupportFragmentManager().beginTransaction().hide(active).show(storageFragment).commit();
+                active = storageFragment;
+                //getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, storageFragment, "STORAGE").commit();
                 return true;
 
             case R.id.recipe_icon:
                 getSupportActionBar().setTitle("Recipe List");
-                RecipeListFragment recipeListFragment = new RecipeListFragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, recipeListFragment, null).commit();
+                getSupportFragmentManager().beginTransaction().hide(active).show(recipeListFragment).commit();
+                active = recipeListFragment;
+
+             //   getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, recipeListFragment, null).commit();
                 return true;
 
             case R.id.cart_icon:
                 getSupportActionBar().setTitle("Shopping List");
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, shoppingListFragment, null).commit();
+                getSupportFragmentManager().beginTransaction().hide(active).show(shoppingListFragment).commit();
+                active = shoppingListFragment;
+                //getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, shoppingListFragment, null).commit();
                 return true;
 
             case R.id.meal_icon:
                 getSupportActionBar().setTitle("Meal Plan");
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, mealPlannerFragment, null).commit();
+                getSupportFragmentManager().beginTransaction().hide(active).show(mealPlannerFragment).commit();
+                active = mealPlannerFragment;
+                //getSupportFragmentManager().beginTransaction().replace(R.id.nav_fragment, mealPlannerFragment, null).commit();
                 return true;
         }
         return false;
