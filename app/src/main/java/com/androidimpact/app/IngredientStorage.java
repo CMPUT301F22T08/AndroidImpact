324a6a4dc6acc9a034522f123cf25cdc4b07be1b
Aneeljyot Alagh <69876558+ajsingh1012@users.noreply.github.com>
2022-11-20 21:26:46 -0700

    Abstracted recipe list and ingredient storage classes to reduce duplicate code and allow for further extensions (#114)
    * abstracted recipe and ingredient lists
    
    * further abstractions, javadocs. Added SortableItemList.OrderedHashMap class.
    
    * applied abstract class to ingredient storage, so that IngredientStorage class now extends abstract class
    
    * remove commented code and clean up code
    
    * uncommented getSortChoice method in SortableItemList class, as it is needed for tests of IngredientStorage

--- app/src/main/java/com/androidimpact/app/IngredientStorage.java
+++ app/src/main/java/com/androidimpact/app/IngredientStorage.java
@@ -1,6 +1,7 @@
 package com.androidimpact.app;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -13,33 +14,34 @@ import java.util.Comparator;
  * @author vedantvyas
  */
 
-public class IngredientStorage {
-    private ArrayList<StoreIngredient> ingredientStorageList;
-
+public class IngredientStorage extends SortableItemList<StoreIngredient> {
+    //private ArrayList<StoreIngredient> ingredientStorageList;
     private static String[] sortChoices;
-    private int sortIndex;
-
-    public static Comparator<StoreIngredient> descriptionComparator, bbdComparator, categoryComparator;
-
 
     /**
      * constructor for IngredientStorage
      */
     public IngredientStorage()
     {
-        this.ingredientStorageList = new ArrayList<StoreIngredient>();
+        super(new ArrayList<>(),
+                new String[]{
+                        "Description",
+                        "Best Before Date",
+                        "Ingredient Category"
+                },
+                (Comparator<StoreIngredient>[]) Arrays.asList(
+                        Comparator.comparing(StoreIngredient::getDescription, String.CASE_INSENSITIVE_ORDER),
+                        Comparator.comparing(StoreIngredient::getBestBeforeCalendar),
+                        Comparator.comparing(StoreIngredient::getCategory, String.CASE_INSENSITIVE_ORDER)
+                ).toArray()
+        );
+        //this.ingredientStorageList = new ArrayList<>();
 
-        this.sortChoices = new String[]{
+        sortChoices = new String[]{
                 "Description",
                 "Best Before Date",
                 "Ingredient Category"
         };
-        this.sortIndex = 0;
-        // set compare variables
-        descriptionComparator = Comparator.comparing(StoreIngredient::getDescription, String.CASE_INSENSITIVE_ORDER);
-        bbdComparator = Comparator.comparing(StoreIngredient::getBestBeforeCalendar);
-        categoryComparator = Comparator.comparing(StoreIngredient::getCategory, String.CASE_INSENSITIVE_ORDER);
-
     }
 
     /**
@@ -47,86 +49,8 @@ public class IngredientStorage {
      * @return ingredientStorageList (ArrayList<StoreIngredient>)
      */
     public ArrayList<StoreIngredient> getIngredientStorageList() {
-        return ingredientStorageList;
+        return super.getData();
     }
-
-    /**
-     * this function returns the element at i index in the list
-     * @param i (int)
-     * @return (StoreIngredient)
-     */
-    public StoreIngredient get(int i) {
-        return ingredientStorageList.get(i);
-    }
-
-    /**
-     * this function sets the ingredients to index i in ingredientStorageList
-     * @param i (int)
-     * @param ingredient (StoreIngredient)
-     */
-    public void set(int i, StoreIngredient ingredient){
-        this.ingredientStorageList.set(i, ingredient);
-    }
-
-    /**
-     * this function adds the ingredient to ingredientStorageList
-     * @param ingredient (StoreIngredient)
-     */
-    public void add(StoreIngredient ingredient)
-    {
-        this.ingredientStorageList.add(ingredient);
-    }
-
-    /**
-     * This function removes the element from ingredientStorageList at index i
-     * @param i (int)
-     */
-    public void remove(int i)
-    {
-        if (i < this.size() && i >= 0)
-            this.ingredientStorageList.remove(i);
-        else
-            throw new ArrayIndexOutOfBoundsException("please choose a i between 0 and list size");
-    }
-
-    /**
-     * This function removes the ingredient from ingredientStorageList
-     * @param ingredient (StoreIngredient)
-     */
-    public void remove(StoreIngredient ingredient)
-    {
-        if (this.ingredientStorageList.contains(ingredient))
-            this.ingredientStorageList.remove(ingredient);
-        else
-            throw new IllegalArgumentException("Trying to remove ingredient which isn't in list");
-    }
-
-    /**
-     * this function returns the size of the list
-     * @return (int)
-     */
-    public int size()
-    {
-        return this.ingredientStorageList.size();
-    }
-
-
-    /**
-     * Return the current sorting choice for the recipe list
-     * @return the index of the sorting choices for the user
-     */
-    public String getSortChoice() {
-        return this.sortChoices[this.sortIndex];
-    }
-
-    /**
-     * Set the sorting choice for the recipe list
-     * @param index the index of the sorting choices for the user
-     */
-    public void setSortChoice(int index) {
-        this.sortIndex = index;
-    }
-
 
     /**
      * Return the sorting choices for the recipe list
@@ -136,26 +60,10 @@ public class IngredientStorage {
         return sortChoices.clone(); }
 
     /**
-     * This function allows us to sort the recipe list by the user's choice
-     */
-    public void sortByChoice() {
-        switch(this.sortIndex) {
-            case 0:
-                Collections.sort(ingredientStorageList, descriptionComparator); break;
-            case 1:
-                Collections.sort(ingredientStorageList, bbdComparator); break;
-            case 2:
-                Collections.sort(ingredientStorageList, categoryComparator); break;
-        }
-    }
-
-    /**
-
-    /**
      * this function clears the ingredientStorageList
      */
     public void clear()
     {
-        this.ingredientStorageList.clear();
+        this.getData().clear();
     }
 }
--- app/src/main/java/com/androidimpact/app/SortableItemList.java
+++ app/src/main/java/com/androidimpact/app/SortableItemList.java
@@ -2,52 +2,65 @@ package com.androidimpact.app;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 /**
- * This class creates the functionality to sort Ingredient and Recipe Lists
+ * This class creates the functionality to have lists with sorting ability
+ * @author Aneeljyot Alagh
  * @version 1.0
  */
-public abstract class SortableItemList {
-    protected ArrayList<Object> objectArrayList;
-    protected static String[] sortChoices;
-    protected int sortIndex;
+public abstract class SortableItemList<T> {
+    protected ArrayList<T> objectArrayList;
+    //protected static String[] sortChoices;
+    private Comparator<T>[] comparators;
+    private OrderedHashMap<String, Comparator<T>> sortingHashMap;
+    private int sortIndex;
 
     /**
      * Constructor for SortableItemList class
      * @param objectArrayList
      * @param sortChoices
      */
-    public SortableItemList(ArrayList<Object> objectArrayList, String[] sortChoices) {
+    public SortableItemList(ArrayList<T> objectArrayList, String[] sortChoices, Comparator<T>[] comparators) {
         this.objectArrayList = objectArrayList;
         this.sortIndex = 0;
-        this.sortChoices = sortChoices;
+        //this.sortChoices = sortChoices;
+        this.comparators = comparators;
+        this.sortingHashMap = new OrderedHashMap<>(sortChoices, comparators);
     }
+
+    /**
+     * This function returns the data in the list
+     * @return list (ArrayList&lt;T&gt;)
+     */
+    public ArrayList<T> getData() { return this.objectArrayList; }
 
     /**
      * this function returns the element at i index in the list
      * @param i (int)
-     * @return (Object)
+     * @return object (T) at position i
      */
-    public Object get(int i) {
+    public T get(int i) {
         return this.objectArrayList.get(i);
     }
 
     /**
-     * this function sets the recipes to index i in SortedItemList
+     * this function sets the object at index i in SortedItemList to a new item
      * @param i (int)
-     * @param object (Object)
+     * @param object (T)
      */
-    public void set(int i, Object object){
+    public void set(int i, T object){
         this.objectArrayList.set(i, object);
     }
 
     /**
-     * this function adds the recipe to SortedItemList
-     * @param object (Object)
+     * this function adds the object to SortedItemList
+     * @param object (T)
      */
-    public void add(Object object)
+    public void add(T object)
     {
-
         this.objectArrayList.add(object);
     }
 
@@ -55,19 +68,19 @@ public abstract class SortableItemList {
      * This function removes the element from sortedItemList at index i
      * @param i (int)
      */
-    public void remove(int i)
+    public void remove(int i) throws ArrayIndexOutOfBoundsException
     {
         if (i < this.size() && i >= 0)
             this.objectArrayList.remove(i);
         else
-            throw new ArrayIndexOutOfBoundsException("please choose a i between 0 and list size");
+            throw new ArrayIndexOutOfBoundsException("Trying to remove item at an index not between 0 and list size");
     }
 
     /**
      * This function removes the recipe from the sortedItemList
      * @param object (Object)
      */
-    public void remove(Object object)
+    public void remove(T object) throws IllegalArgumentException
     {
         if (this.objectArrayList.contains(object))
             this.objectArrayList.remove(object);
@@ -89,7 +102,8 @@ public abstract class SortableItemList {
      * @return the sorting choice at the index chosen by the user
      */
     public String getSortChoice() {
-        return this.sortChoices[this.sortIndex];
+        //return this.sortChoices[this.sortIndex];
+        return this.sortingHashMap.getKey(this.sortIndex);
     }
 
     /**
@@ -104,12 +118,72 @@ public abstract class SortableItemList {
      * Return the sorting choices for the item list
      * @return list of available sorting choices
      */
-    public static String[] getSortChoices() {
-        return sortChoices.clone();
-    }
+//    public static String[] getSortChoices() {
+//        return sortChoices.clone();
+//    }
 
     /**
      * This function allows us to sort the item list by the user's choice
      */
-    public abstract void sortByChoice();
+    public void sortByChoice() {
+        Collections.sort(this.objectArrayList, this.sortingHashMap.getValue(this.sortIndex));
+    }
+
+    /**
+     * This class creates a version of a hashmap that maintains an order.
+     * @param <K> The key type
+     * @param <V> The value type
+     */
+    private class OrderedHashMap<K, V> extends HashMap<K, V> {
+        private K[] keys;
+
+        /**
+         * Initialize a hashmap that contains a natural ordering.
+         * <br>
+         * <strong>NB:</strong> This structure requires that the length of keys given and values
+         * given are equal, and the i'th key corresponds to the i'th value in the list.
+         * <strong>NB:</strong> If two keys are given that are equal, the hashmap will only consider
+         * the latter of these in the keys array, i.e. the key with the maximal i-value.
+         * @param keys an array of keys to be added
+         * @param values an array of values to be added
+         * @throws IllegalArgumentException if the arrays of keys and values have different sizes
+         */
+        public OrderedHashMap(K[] keys, V[] values) throws IllegalArgumentException {
+            super();
+            if(keys.length == values.length) {
+                this.keys = keys;
+                for(int i = 0; i < keys.length; i++) {
+                    super.put(keys[i], values[i]);
+                }
+            } else {
+                throw new IllegalArgumentException("Incorrect arguments, too many " + (keys.length > values.length ? "keys": "values") + "given.");
+            }
+        }
+
+        /**
+         * Returns the i'th key in the hashmap.
+         * @param i the index of the key in the hashmap
+         * @return the i'th key in the hashmap (K)
+         */
+        public K getKey(int i) {
+            return keys[i];
+        }
+
+        /**
+         * Returns the i'th value in the hashmap.
+         * @param i the index of the value in the hashmap
+         * @return the value of the i'th key in the hashmap (V)
+         */
+        public V getValue(int i) {
+            return super.get(keys[i]);
+        }
+
+        /**
+         * Returns a copy of the keys in the OrderedHashMap
+         * @return an array of the keys (K[])
+         */
+        public K[] getKeys() {
+            return keys.clone();
+        }
+    }
 }
