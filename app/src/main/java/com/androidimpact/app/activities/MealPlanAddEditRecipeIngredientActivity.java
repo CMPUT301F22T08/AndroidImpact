8016622d9dc8c587f7581072134e7f7d0c055ca2
Aneeljyot Alagh <69876558+ajsingh1012@users.noreply.github.com>
2022-11-24 00:16:27 -0700

    Merge meal plan edits with main, include edit button to open add/edit meal plan activity (#139)
    * added partial functionality to meal plan adding/editing
    
    * small edit meal plan change
    
    Co-authored-by: Clare Chen <clarechen@Clares-Air.hitronhub.home>

---
+++ app/src/main/java/com/androidimpact/app/activities/MealPlanAddEditRecipeIngredientActivity.java
@@ -0,0 +1,16 @@
+package com.androidimpact.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.androidimpact.app.R;
+
+public class MealPlanAddEditRecipeIngredientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_plan_add_edit_recipe_ingredient);
+    }
+}
