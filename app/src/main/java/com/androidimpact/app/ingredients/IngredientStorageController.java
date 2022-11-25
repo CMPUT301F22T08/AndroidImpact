1455ba449064a3e0c0cf05bf5724a5af0005822e
Kailash Seshadri <87671876+kailashs2002@users.noreply.github.com>
2022-11-24 21:34:29 -0700

    Javadocs

--- app/src/main/java/com/androidimpact/app/ingredients/IngredientStorageController.java
+++ app/src/main/java/com/androidimpact/app/ingredients/IngredientStorageController.java
@@ -13,6 +13,10 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * Controller Class for IngredientStorage
+ * Acts as a wrapper around IngredientStorage and implements functionality to back up the storage on FireStore
+ */
 public class IngredientStorageController {
     final String TAG = "IngredientStorageController";
     final String firestorePath = "ingredientStorage";
@@ -22,7 +26,11 @@ public class IngredientStorageController {
     private CollectionReference ingredientStorageCollection;
     private IngredientStorage ingredientStorage;
 
-
+    /**
+     * Constructor: Creates an empty IngredientStorage class and populates it with data from FireStore
+     * @param context
+     *      (Context) The current context, used to push success/failure SnackBars to the screen.
+     */
     public IngredientStorageController(Context context) {
         this.context = context;
         db = FirebaseFirestore.getInstance();
@@ -30,11 +38,21 @@ public class IngredientStorageController {
         ingredientStorage = new IngredientStorage();
     }
 
+    /**
+     * Creates a SnackBar that pup-up on the screen
+     * @param s (String) - The text to be shown in the SnackBar
+     */
     private void pushSnackBarToContext(String s) {
         Snackbar.make(((MainActivity)context).findViewById(R.id.nav_fragment), s, Snackbar.LENGTH_LONG)
                 .setAction("OK", (v)->{}).show();
     }
 
+    /**
+     * Add a snapshot listener to the FireStore collection that repopulates the ingredient storage when changes are detected.
+     * Notifies the current ViewAdapter that the data may have changed on .
+     * @param storeIngredientViewAdapter
+     *      (StoreIngredientViewAdapter) - The adapter to be updated when the latest data is pulled from firestore
+     */
     public void addDataUpdateSnapshotListener(StoreIngredientViewAdapter storeIngredientViewAdapter){
         ingredientStorageCollection.addSnapshotListener((queryDocumentSnapshots, error) -> {
             if (error != null) {
@@ -71,6 +89,13 @@ public class IngredientStorageController {
         });
     }
 
+    /**
+     * Adds/updates a storeIngredient object to the FireStore database.
+     * Updates if the id of the parameter StoreIngredient object is not null.
+     * Else generates a random ID and adds the item to the database
+     * @param storeIngredient
+     *      (StoreIngredient) - The ingredient to be added/edited in storage
+     */
     public void addEdit(StoreIngredient storeIngredient){
         // Adds if id is null else edits
         String id = storeIngredient.getId();
@@ -82,7 +107,12 @@ public class IngredientStorageController {
         ingredientStorageCollection.document(id).set(storeIngredient);
     }
 
-    public void delete(int position) {
+    /**
+     * Deletes the object in storage at the parameter position
+     * @param position (int) - The position at which to delete the item
+     * @throws ArrayIndexOutOfBoundsException if the index is not within the size of the storage
+     */
+    public void delete(int position) throws ArrayIndexOutOfBoundsException{
         // Get the swiped item at a particular position.
         StoreIngredient deletedIngredient = ingredientStorage.get(position);
         String description = deletedIngredient.getDescription();
@@ -103,13 +133,26 @@ public class IngredientStorageController {
                 });
     }
 
-    public String[] getSortingChoices() { return ingredientStorage.getSortChoices();}
+    /**
+     * Get an list of names (String) that the storage can be sorted on
+     * @return String[] - The sorting choices
+     */
+    public String[] getSortingChoices() { return IngredientStorage.getSortChoices();}
 
+    /**
+     * Sort the data according to parameter choice.
+     * @param sortChoice - The position in the SortingChoices list that you want to sort by
+     * @see com.androidimpact.app.SortableItemList
+     */
     public void sortData(int sortChoice){
         ingredientStorage.setSortChoice(sortChoice);
         ingredientStorage.sortByChoice();
     }
 
+    /**
+     * Returns an array of StoreIngredient objects that are in the IngredientStorage associated with the controller
+     * @return ArrayList<StoreIngredient>
+     */
     public ArrayList<StoreIngredient> getData(){
         return ingredientStorage.detData();
     }
