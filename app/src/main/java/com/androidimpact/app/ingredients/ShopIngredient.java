ef9a1ae48aa3578963db364eddeffa48c757926e
Vedant Vyas <53002423+vedudx@users.noreply.github.com>
2022-11-24 19:45:45 -0700

    Shopping List Preview (#140)
    * Shopping List Preview
    
    * improving the UI of shopping list item
    
    * final UI fixes
    
    * Increasing padding
    
    * creating shopIngredient

---
+++ app/src/main/java/com/androidimpact/app/ingredients/ShopIngredient.java
@@ -0,0 +1,34 @@
+package com.androidimpact.app.ingredients;
+
+import java.io.Serializable;
+
+public class ShopIngredient extends Ingredient implements Serializable {
+
+    private float amountPicked = 0;
+
+
+    /**
+     * Empty constructor for firebase
+     */
+    public ShopIngredient() {}
+
+    //will create two instructors one where amount picked is not specified
+    public ShopIngredient(String id, String description, float amount, String unit, String category) {
+        super(id, description, amount, unit, category);
+        this.amountPicked=0;
+    }
+
+    public ShopIngredient(String id, String description, float amount, String unit, String category, float amountPicked) {
+        super(id, description, amount, unit, category);
+        this.amountPicked=amountPicked;
+    }
+
+    public float getAmountPicked() {
+        return amountPicked;
+    }
+
+    public void setAmountPicked(float amountPicked) {
+        this.amountPicked = amountPicked;
+    }
+
+}
