7a359125f306a888655dfe641172bc47031f65bf
Aneeljyot Alagh <69876558+ajsingh1012@users.noreply.github.com>
2022-11-27 19:11:38 -0700

    added small import

--- app/src/main/java/com/androidimpact/app/activities/LoginActivity.java
+++ app/src/main/java/com/androidimpact/app/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package com.androidimpact.app.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
